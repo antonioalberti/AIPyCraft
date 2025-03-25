39c9ca5635844c3ffeaad6eba6ae31704fa5024d
Alexis Leal <alexisleal@gmail.com>
2025-03-25 17:36:06 -0400

    Adiciona logger.py com geração de logs

---
+++ utils/logger.py
@@ -0,0 +1,22 @@
+# logger.py
+import logging
+import os
+from datetime import datetime
+
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+log_filename = datetime.now().strftime("log_%Y-%m-%d_%H-%M-%S.log")
+log_path = os.path.join(LOG_DIR, log_filename)
+
+# Configuração do logger
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(log_path),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger("AIPyCraftLogger")
